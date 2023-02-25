47b6c77d7866603a29432aa43cccb940a6712e3c
Ruben Guzman <rubendario981@hotmail.com>
2023-02-24 22:13:39 -0500

    Page that shows list episodes and theirs details is done

--- client/src/app/pages/detail-episode/detail-episode.component.ts
+++ client/src/app/pages/detail-episode/detail-episode.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Episode } from 'src/app/models/ModelEpisode';
+import { EpisodesService } from 'src/app/services/episodes.service';
 
 @Component({
   selector: 'app-detail-episode',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./detail-episode.component.css']
 })
 export class DetailEpisodeComponent {
+  public episode!: Episode;
+  public id!: number;
+  
+  constructor(
+    private requestEpisode: EpisodesService,
+    private routeActive: ActivatedRoute
+  ){
 
+  }
+
+  ngOnInit(){
+    this.routeActive.params.subscribe((params: Params)=>{
+      this.id = params["id"]
+    })
+    this.requestEpisode.getDetailEpisode(this.id).subscribe(
+      response => this.episode = response
+    )
+  }
 }
